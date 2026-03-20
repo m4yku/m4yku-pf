a11c76028fb9b512a7463b4280a8f17dc1c67d9f
m4yku <hbdmike23@gmail.com>
2026-03-20 11:22:01 +0800

    feat: replace favicon.png with favicon.ico for improved compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -2,19 +2,20 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			// FORCE base path - walang condition!
-			base: '/m4yku-pf'
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
+    appDir: 'app', // <--- ITO YUNG NAGBABALIK NA MAGIC FIX!
+    paths: {
+      // FORCE base path - walang condition!
+      base: '/m4yku-pf'
+    }
+  }
 };
 
 export default config;
